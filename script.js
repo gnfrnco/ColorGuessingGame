da15a50246531eb31448dc0fc87269e23bd2ce71
Gian Lazaro <gianlazaro@protonmail.com>
2017-02-08 00:53:04 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var red, green, blue;
+newGame();
+
+var answer = "rgb(" + red + "," + green + "," + blue + ")";
+var rgbSign = document.querySelector('h1');
+
+rgbSign.textContent = answer;
+
+
+var randomizer = '.choice' + assignBoxNum();
+
+document.querySelector(randomizer).style.backgroundColor = answer;
+
+function getRandom(){ return Math.round(Math.random() * 255); }
+
+function newGame(){
+red = getRandom();
+green = getRandom();
+blue = getRandom();  
+}
+
+function assignBoxNum(){
+  let min = 1, max = 6;
+  let random = Math.round(Math.random()*(max-min) + min);
+ console.log(random);
+  return random;
+}
